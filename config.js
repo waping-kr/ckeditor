743aa738fb5584508c4cc8b26c53aaefbfc03fc2
Waping <119610040+waping-kr@users.noreply.github.com>
2024-06-24 02:45:56 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function(config) {
+    config.language = "ko";
+    // config.font_names =
+    //     "와핑;";
+    config.contentsCss = [
+        "/js/ckeditor/contents.css"
+    ];
+
+    console.log(config.contentsCss);
+};
